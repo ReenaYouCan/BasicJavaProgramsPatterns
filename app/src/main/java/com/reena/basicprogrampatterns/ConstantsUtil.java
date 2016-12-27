0e6fc49cde797f1bb84061f2db936ffbc7281bf8
reena_nachare_sci <reena.nachare@scispl.com>
2016-12-27 17:17:25 +0530

    Base Structure

---
+++ app/src/main/java/com/reena/basicprogrampatterns/ConstantsUtil.java
@@ -0,0 +1,23 @@
+package com.reena.basicprogrampatterns;
+
+/**
+ * Created by scispl15 on 27-12-2016.
+ */
+
+public class ConstantsUtil {
+
+//    https://www.guvi.in/blogs/attending-the-interview-you-dont-need-to-be-100-perfect-neither-you-cant-fake-it-100/
+
+//  http://www.java2novice.com/java-interview-programs/
+
+//    https://www.guvi.in/blogs/top-10-java-programs-for-freshers/
+
+    public String[] programNumber = {"Prime Number", "Factorial Number", "Largest And Smallest Number from Array", "Reverse  Number", "Swap Number with/without third variable", ""};
+    //Reference http://www.instanceofjava.com/2015/04/java-interview-programs-on-strings.html
+    public String[] programString = {"Reverse String", "Sort String", "Polimdrome String", "Reverse  Number", "Swap Number with/without third variable", ""};
+
+    // Patterns
+    public String[] programPatterns = {"Reverse String", "Sort String", "Polimdrome String", "Reverse  Number", "Swap Number with/without third variable", ""};
+
+
+}
